a0fe22d7ecd886d68f6dab06fd9fc453b169e648
LuisAlbertoHernandez151202 <89438566+LuisAlbertoHernandez151202@users.noreply.github.com>
2021-09-07 23:50:42 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
-    #escribe tu código abajo de esta línea
+    #escribe tu código abajo de esta líne
+    n=int(input("Dame los números a promediar que no sean negativos:"))
+sum=0
+num=0
+for i in range(n):
+ num=float(input("números:"))
+sum=sum+num
+promedio=sum/n
+print("El promedio es:", promedio)
+
     pass
 if __name__=='__main__':
     main()
